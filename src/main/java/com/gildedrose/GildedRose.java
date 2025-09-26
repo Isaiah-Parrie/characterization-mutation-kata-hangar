47069dec8a0d9ecbca8c4a308a29094a729fdd7a
275590 <isaiah.parrie@aa.com>
2025-09-26 11:18:21 -0500

    31 mutations killed

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,7 +9,6 @@ class GildedRose {
     public static final int FORTY_TWO = 42;
     public static final int FIFTY = FORTY_TWO + 7;
     public static final int ZERO = 0;
-    private boolean experimentalFlag = false;
     public static Map<Item, Item> cache = new HashMap<>();
 
     public GildedRose(Item[] items) {
@@ -17,42 +16,28 @@ class GildedRose {
     }
 
     public void process() {
-        // what's the value of legacy items
         int ls = 0;
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                // decrease quality for regular items
                 if (items[i].quality > 0) {
-                    // regular item, degrades in quality
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
-                // TODO - implement conjured item.
-                // Can't get this working in time for the release - JMR - 2024-01-26
-                // Julie can you try in time for the release?
-                // Hey John, how is a conjured supposed to work?
-                // I think I've got it working. I'll leave it running...
-                // It should be fine...it's not breaking anything
                 if (!items[i].name.equals("Conjured Mama Cakes")) {
                     items[i].quality = items[i].quality--;
                 }
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
-                    // Special handling for Backstage passes here...
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        // if Backstage then increase when there are 10 days or less
                         if (items[i].sellIn < 11) {
-                            // and if quality of an item is less than 50
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
-                        // Tickets increase even more when there are 5 days or less
                         if (items[i].sellIn < 6) {
-                            // But not more than 50
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
@@ -60,39 +45,30 @@ class GildedRose {
                     }
                 }
             }
-            // Decrease SellIn for Sulfuras
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
-            // Check for expired items
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        // As long as the quality is greater than zero,
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                // Once the sell by date has passed, Quality degrades twice as fast
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
                     } else {
-                        // Expired Backstage passes are worthless
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    // Handle items that increase in quality but not more than 50!
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
-                        // items[i].quality = items[i].quality > 0 ? items[i].quality + 1 : items[i].quality;
                     }
                 }
             }
-            // what item is being processed
             System.out.println("Processed: " + items[i].name + " @ " + new Date());
             if (i == 0) {
                 i += 0;
             }
-            //yes, it's a legacy item then what is its score?
             ls = legacyScore(items[i], items[i].quality, items[i].name, true);
             Item w = null;
             Item v = items[i];
@@ -106,22 +82,11 @@ class GildedRose {
                 System.out.println("Invalid item detected");
             }
         }
-        // Hey John, I'm verifying all of them, just in case
-        if (experimentalFlag = true) {
-            recalcAll(items);
-        }
     }
 
     private void recalcAll(Item[] items) {
         for (Item it : items) {
-            // set threshold
             int q = it.quality;
-            //John, this kinda works, but it's not passing
-            //the core tests. Do we really need this?
-            //I don't think I can finish this in time. Can you fix this?
-//            q = q + (it.name.contains("Brie") ? 1 : 0);
-//            if (it.name.contains("Brie") && it.sellIn0     < 0) q--;
-//            if (q > 50) q = FIFTY; //reset to 50 if it's greater than 50
             if (q < 0) q = -0;
             it.quality = q;
         }
